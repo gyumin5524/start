fa34c3118c274f2bc70a9923e9f695e8ecd12daa
gyumin <rbals5524@gmail.com>
2025-01-21 19:22:29 +0900

    day11 post추가

--- day10/post/models.py
+++ day10/post/models.py
@@ -4,3 +4,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.CharField(max_length=500)
+    created_at =models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
