9c10778d738b64ef61d22a9230c5f78c6c5d5b4a
gyumin <rbals5524@gmail.com>
2025-01-27 12:29:46 +0900

    day12 day13추가

--- day10/post/models.py
+++ day10/post/models.py
@@ -2,11 +2,18 @@
 # 데이터베이스 테이블을 정의하는 곳입니다.
 # Django의 ORM(Object-Relational Mapping)을 사용하여 테이블과 필드 정의를 작성합니다.
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50)
+
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
-    content = models.CharField(max_length=500)
+    content = models.TextField(max_length=500, null=True)
     created_at =models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    tags = models.ManyToManyField(Tag)
+
--- day10/user/models.py
+++ day10/user/models.py
@@ -3,5 +3,10 @@
 # 사용자와 관련된 데이터 구조를 설계합니다.
 # 기본 사용자 모델(AbstractUser 또는 AbstractBaseUser)을 커스터마이징하여 확장할 때 사용됩니다.
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField()
