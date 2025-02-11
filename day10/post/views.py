2809323b241d07914f45e74487aec93565e399bf
gyumin <rbals5524@gmail.com>
2025-02-11 16:34:06 +0900

    주석추가 및 get_object_or_404수정

--- day10/post/views.py
+++ day10/post/views.py
@@ -8,6 +8,7 @@ from rest_framework import status
 from .models import Post
 from .serializers import PostSerializer
 
+# READ
 class PostList(APIView):
     def get(self, request):
         posts = Post.objects.all()
@@ -20,12 +21,13 @@ class PostList(APIView):
 class PostDetail(APIView):
     def get(self, request, pk):
         # 처리 - id 값을 이용해서 상세페이지 글 가져오기
-        post = Post.objects.get(id = pk)
+        post = Post.objects.get_object_or_404(id = pk)
         serializer = PostSerializer(post)
         
         return Response(serializer.data, status=status.HTTP_200_OK)
+
         
-        
+# CREATE        
 class PostWrite(APIView):
     def post(self, request):
         #응답 역직렬화, json데이터 -> raw데이터 
@@ -37,6 +39,7 @@ class PostWrite(APIView):
         return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
 
 
+# UPDATE
 class PostUpdate(APIView):
     def put(self, request, pk):
         # 전체수정
@@ -44,7 +47,7 @@ class PostUpdate(APIView):
         # 2. 어떻게 수정해야할 지 알아야 한다.
         # 3. 실제로 수정
         # 4. 저장
-        post = Post.objects.get_object_or_400(id = pk) # <-디비
+        post = Post.objects.get_object_or_404(id = pk) # <-디비
         data = request.data # <- 사용자한테서
         serializer = PostSerializer(post, data=data) # 직렬화, 역직렬화 양방향
         if serializer.is_valid():
@@ -53,20 +56,21 @@ class PostUpdate(APIView):
         return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
     
     def patch(self, request, pk):
-        post = Post.objects.get_object_or_400(id = pk) # <-디비
+        post = Post.objects.get_object_or_404(id = pk) # <-디비
         data = request.data # <- 사용자한테서
         serializer = PostSerializer(post, partial=data) # partial 부분수정
         if serializer.is_valid():
             serializer.save() #True, 저장
             return Response(serializer.data, status = status.HTTP_201_CREATED)
         return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+ 
         
-
+# DELETE
 class PostDelete(APIView):
     def delete(self, request, pk):
         # 처리 - id값을 이용해서 삭제
         # 1. id값을 통해서 해당 객체 가져오기
-        post = Post.objects.get(id = pk)
+        post = Post.objects.get_object_or_404(id = pk)
         # 2. 삭제하기 
         post.delete()
     
