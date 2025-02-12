93a6480974e7df1aa7f376406e9937513ed21142
gyumin <rbals5524@gmail.com>
2025-02-12 12:07:41 +0900

    tag import

--- day10/post/serializers.py
+++ day10/post/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
-from .models import Post
+from .models import Post, Tag
 
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
+        fields = '__all__'
+
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
         fields = '__all__'
